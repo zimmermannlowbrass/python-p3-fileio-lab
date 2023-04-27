bdd693e950e85e465e7ffb6c1ffbb9e86f1e3c0e
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-27 08:59:08 -0500

    completed labwork

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as file:
+        return file.read()
